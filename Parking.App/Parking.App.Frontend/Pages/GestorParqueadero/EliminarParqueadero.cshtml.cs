f2206853879af77e025d0a6d92162c14f9b2205a
Pablo Mayorga <pablomayorgqa@gmail.com>
2021-10-10 02:35:07 -0500

    Tengo sueño 2

--- Parking.App/Parking.App.Frontend/Pages/GestorParqueadero/EliminarParqueadero.cshtml.cs
+++ Parking.App/Parking.App.Frontend/Pages/GestorParqueadero/EliminarParqueadero.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Parking.App.Persistencia;
+using Parking.App.Dominio;
 
 namespace Parking.App.Frontend
 {
